a294f9bf8660110d0dbbba4492c63a544c3d44bf
spantheslayer <supriyosarkar282@gmail.com>
2020-08-02 11:52:59 +0530

    Revert "deleted counter component and modiefied app.js"
    This reverts commit 8a30f511083663114b136801b91f316a833b9287.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
+import Counter from "./components/counter";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById("root")
 );
